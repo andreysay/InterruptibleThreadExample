3579e8e393674c2258a5377461662e921914eff8
Andrey Spitsyn <andreysay@live.com>
2018-12-06 12:11:33 +0300

    Added number of running threads calculation.

--- InterruptibleThreadExample/InterruptibleThreadExample.cpp
+++ InterruptibleThreadExample/InterruptibleThreadExample.cpp
@@ -5,7 +5,9 @@
 
 std::atomic<unsigned int> counter;
 std::vector<InterruptibleThread> backgroundThreads;
-static unsigned int threadNum = 3;
+unsigned long max_threads = 4;
+unsigned long const hardware_threads = std::thread::hardware_concurrency();
+unsigned long const num_threads = std::min(hardware_threads != 0 ? hardware_threads : 2, max_threads);
 std::mutex procMutex;
 
 thread_local bool done = false;
@@ -29,7 +31,7 @@ void foo()
 
 void start_background_processing()
 {
-    for (size_t i = 0; i < threadNum; i++)
+    for (size_t i = 0; i < max_threads; i++)
     {
         backgroundThreads.push_back(InterruptibleThread(foo));
     }
@@ -37,11 +39,13 @@ void start_background_processing()
 
 int main()
 {
+    std::cout << "Max threads on this computer is: " << max_threads << std::endl;
+    std::cout << "Hardware threads detected: " << hardware_threads << std::endl;
     start_background_processing();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-    for (size_t i = 0; i < threadNum; i++)
+    for (size_t i = 0; i < max_threads; i++)
     {
         backgroundThreads[i].interrupt();
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
